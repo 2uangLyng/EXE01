f61f2dd6c5b9187fc8dde145638b45c0ee34db54
Võ Quang Linh <21dh111007@st.huflit.edu.vn>
2025-03-31 14:45:44 +0700

    fix image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["images.unsplash.com", "cdn.pixabay.com", "cdn.shopify.com", "storage.googleapis.com"],
+  },
 };
 
 export default nextConfig;
